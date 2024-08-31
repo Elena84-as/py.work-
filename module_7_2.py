cda79d3e0d36b5c498bf4ce5b20dc150abaa072c
Elena84-as <evamaisel@yandex.ru>
2024-08-31 22:28:45 +0300

    Add files via upload

---
+++ module_7_2.py
@@ -0,0 +1,26 @@
+import io
+from pprint import pprint
+
+
+
+def custom_write(file_name, strings):
+    strings_positions = {}
+    with open(file_name, 'w', encoding='utf-8') as file:
+        for i, j in enumerate(strings,start = 0):
+            key = (i+1, file.tell())
+            strings_positions[key] = j
+            file.write(f'{j} \n')
+        file.close()
+    return strings_positions
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
